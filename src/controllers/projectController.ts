1b6ac8cd3a9e86131ba36bbe2ee0e4d3720bde5f
Niklas-Nordin <niklas.nordin@chasacademy.se>
2025-09-26 14:35:55 +0200

    Adding getProjects and getProjectsById

--- src/controllers/projectController.ts
+++ src/controllers/projectController.ts
@@ -44,3 +44,70 @@ export const createProject = async (req: ProtectedRequest, res: Response) => {
     res.status(500).json({ message: "Server error", error });
   }
 };
+
+export const getAllProjects = async (req: ProtectedRequest, res: Response) => {
+  try {
+    const userId = req.user?.id;
+
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const projects = await Project.find({
+      $or: [{ admin: userId}, {members: userId }]
+    })
+    .populate("admin", "-password")
+    .populate("members", "-password")
+    .populate("tasks");
+
+    res.status(200).json({ message: "Projects retrieved", projects });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
+
+export const getProjectById = async (req: ProtectedRequest, res: Response) => {
+  try {
+    const userId = req.user?.id;
+    const projectId = req.params.id;
+
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    if (!projectId || !Types.ObjectId.isValid(projectId)) {
+      return res.status(400).json({ message: "Invalid project ID" });
+    }
+
+    const project = await Project.findById(projectId)
+      .populate("admin", "-password")
+      .populate("members", "-password")
+      .populate("tasks");
+    
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    if (project.admin.id.toString() !== userId && !project.members.map(m => m.id.toString()).includes(userId)) {
+      return res.status(403).json({ message: "Access denied" });
+    }
+
+    res.status(200).json({ message: "Project retrieved", project });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
+
+//TODO: update project
+const updateProject = async (req: ProtectedRequest, res: Response) => {
+  try {
+    const userId = req.user?.id;
+    
+  } catch (error) {
+    
+  }
+};
+
+
+// TODO: delete project
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,11 @@
-import { createProject } from "../controllers/projectController.js";
+import { createProject, getAllProjects, getProjectById } from "../controllers/projectController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import { Router } from "express";
 
 const router = Router();
 
+router.get("/", authMiddleware, getAllProjects);
 router.post("/create", authMiddleware, createProject);
+router.get("/:id", authMiddleware, getProjectById);
 
 export default router;
