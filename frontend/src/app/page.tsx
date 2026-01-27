2ab521649eff30be283b59ab8329c1cd843daf06
Niklas-Nordin <niklas.nordin@chasacademy.se>
2026-01-27 15:55:28 +0100

    Test commit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,8 +4,8 @@ import Hero from "../components/landingPage/Hero";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Hero />
-    </div>
+    </>
   );
 }
--- frontend/src/components/landingPage/Hero.tsx
+++ frontend/src/components/landingPage/Hero.tsx
@@ -7,6 +7,7 @@ function Hero() {
         <img className={styles.kanbanImage} src="./kanban-image.png" alt="A kanban image" />
         <h2 className={styles.subtitle}>Keep your team together by organizing projects and tasks in one place</h2>
         <button className={`${styles.getStartedButton} ${styles.button}`}>Get started</button>
+        
     </div>
   );
 }
