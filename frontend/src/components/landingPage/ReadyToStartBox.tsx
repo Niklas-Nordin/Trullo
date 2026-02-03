ae66785f68147c65c1e47e8707c9fa93cc625c30
Niklas-Nordin <niklas.nordin@chasacademy.se>
2026-02-03 17:06:14 +0100

    Ready to start box is done

--- frontend/src/components/landingPage/ReadyToStartBox.tsx
+++ frontend/src/components/landingPage/ReadyToStartBox.tsx
@@ -4,8 +4,8 @@ import GetStartedButton from "./GetStartedButton";
 function ReadyToStartBox() {
   return (
     <div className={styles.container}>
-        <h2>Ready to start?</h2>
-        <p>Join over 10 000+ projects using Trullo</p>
+        <h2 className={styles.title}>Ready to start?</h2>
+        <p className={styles.description}>Join over 10 000+ projects using Trullo</p>
         <GetStartedButton size="small" />
     </div>
   );
