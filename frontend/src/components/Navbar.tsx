5131a0dc668cd0d5c154649a8ed74d4e1ee5d816
Niklas-Nordin <niklas.nordin@chasacademy.se>
2026-02-04 15:21:03 +0100

    Removing hamburger meny navigation bar if width changes to 768px while open. Also Adding the navmenu

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,10 @@
+"use client"
+
 import Link from "next/link";
 import style from "./navbar.module.css"
 import LogoutButton from "./auth/LogoutButton";
+import { useEffect, useState } from "react";
+import { div } from "framer-motion/client";
 
 type Props = {
   isLoggedIn: boolean;
@@ -8,24 +12,47 @@ type Props = {
 
 function Navbar({ isLoggedIn }: Props) {
 
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      if(window.innerWidth >= 768) {
+        setMenuOpen(false)
+      }
+    }
+
+    window.addEventListener("resize", handleResize)
+
+    return () => removeEventListener("resize", handleResize)
+  }, [])
+
   return (
     <div className={style.navbarContainer}>
       <Link className={style.logo} href="/"><img className={style.logo} src="/FlowLine.png" alt="FlowLine logo" /></Link>
-      {!isLoggedIn && (
-        <>
-          <Link href="/">Home</Link>
-          <Link href="/">About</Link>
-          <Link href="/">Contact</Link>
-        </>
-      )}
-      {isLoggedIn && (
-        <>
-          <Link href="/dashboard">Dashboard</Link>
-          <Link href="/projects">Projects</Link>
-          <LogoutButton />
-        </>
-      )}
+      <div className={`${style.navLinks} ${menuOpen ? style.open : ""}`}>
+        <img className={style.close} src="./close.svg" alt="Close X mark" onClick={() => setMenuOpen(false)} />
+        {!isLoggedIn && (
+          <>
+            <Link href="/" onClick={() => setMenuOpen(false)}>Home</Link>
+            <Link href="/" onClick={() => setMenuOpen(false)}>About</Link>
+            <Link href="/" onClick={() => setMenuOpen(false)}>Contact</Link>
+          </>
+        )}
+        {isLoggedIn && (
+          <>
+            <Link href="/dashboard" onClick={() => setMenuOpen(false)}>Dashboard</Link>
+            <Link href="/projects" onClick={() => setMenuOpen(false)}>Projects</Link>
+            <LogoutButton />
+          </>
+        )}
+      </div>
       <Link href="/auth"><img className={style.profileButton} src="/user-profile-icon.svg" alt="Profile icon" /></Link>
+      <button className={style.hamburgerMenu} onClick={() => setMenuOpen(prev => !prev)}>
+        <img src="./hamburger-menu.svg" alt="Hamburger menu for navigation links" />
+      </button>
+
+      { menuOpen && <div className={style.overlay} onClick={() => setMenuOpen(false)}/>
+      }
     </div>
   );
 }
